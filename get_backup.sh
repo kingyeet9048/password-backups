9752a76c9d9dc14774931c77594a684a73854603
Sulaiman Bada <sulaimanbada8048@gmail.com>
2022-10-25 04:13:16 +0000

    Most Recent backup: Tue Oct 25 04:13:16 UTC 2022

--- get_backup.sh
+++ get_backup.sh
@@ -3,19 +3,19 @@
 snap install bw
 apt-get install logrotate -y
 
-sudo export $(grep -v '^#' .env | xargs)
+export $(grep -v '^#' .env | xargs)
 
-sudo bw login --apikey
+bw login --apikey
 
 backup_dir=$(date +'%m-%d-%Y')
-sudo -u ubuntu sh -c "echo $BW_PASSWORD | bw export --output backups/output-${backup_dir} --format encrypted_json --password $BW_PASSWORD"
+echo $BW_PASSWORD | bw export --output backups/output-${backup_dir} --format encrypted_json --password $BW_PASSWORD
 
 chmod 100 backups/output-${backup_dir}
 chown "root:root" backups/output-${backup_dir}
 
-sudo unset BW_CLIENTID BW_CLIENTSECRET BW_PASSWORD
+unset BW_CLIENTID BW_CLIENTSECRET BW_PASSWORD
 
-sudo bw logout
+bw logout
 
 git add .
 git commit -m "Most Recent backup: $(date)"
