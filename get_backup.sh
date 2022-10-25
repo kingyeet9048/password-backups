bf17e5b16728ac4871a1d5491aafdf691f4e8f94
Sulaiman Bada <sulaimanbada8048@gmail.com>
2022-10-25 04:25:16 +0000

    Most Recent backup: Tue Oct 25 04:25:16 UTC 2022

--- get_backup.sh
+++ get_backup.sh
@@ -8,10 +8,10 @@ export $(grep -v '^#' .env | xargs)
 bw login --apikey
 
 backup_dir=$(date +'%m%d%Y')
-echo $BW_PASSWORD | bw export --output backups/output-${backup_dir} --format encrypted_json --password $BW_PASSWORD
+echo $BW_PASSWORD | bw export --output backups/output-${backup_dir} --format encrypted_json --password $BW_PASSWORD -v
 
 #chmod 100 backups/output-${backup_dir}
-chown "root:root" /etc/password-backups/backups/output-${backup_dir}
+#chown "root:root" /etc/password-backups/backups/output-${backup_dir}
 
 unset BW_CLIENTID BW_CLIENTSECRET BW_PASSWORD
 
