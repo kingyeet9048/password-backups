e1ce354a0471436681a96d145b09c21e2f0f4dbf
An Author <anemail.com>
2022-10-24 22:42:31 -0500

    initial commit

---
+++ get_backup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+apt-get install bitwarden-cli logrotate -y
+
+export $(grep -v '^#' .env | xargs)
+
+bw login --apikey
+
+backup_dir=$(date +'%m-%d-%Y')
+echo $BW_PASSWORD | bw export --output backups/output-${backup_dir} --format encrypted_json --password $BW_PASSWORD
+
+chmod 100 backups/output-${backup_dir}
+chown "ubuntu:ubuntu" backups/output-${backup_dir}
+
+unset BW_CLIENTID BW_CLIENTSECRET BW_PASSWORD
+
+bw logout
+
+git add .
+git commit -m "Most Recent backup: $(date)"
+git push origin master
+
+cp passwords.conf /etc/logrotate.d/
+logrotate -d /etc/logrotate.d/passwords.conf
